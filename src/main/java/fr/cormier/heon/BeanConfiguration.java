dffb5fb07fcb1b044534e46eaee8d5cb6e9d419b
guillaume Cormier <cormier.guillaume83@gmail.com>
2019-12-19 14:35:56 +0100

    Bean heonLight + prise en compte properties

--- src/main/java/fr/cormier/heon/BeanConfiguration.java
+++ src/main/java/fr/cormier/heon/BeanConfiguration.java
@@ -2,19 +2,22 @@ package fr.cormier.heon;
 
 
 import fr.cormier.heonLight.BeanConfigurationHeonPixel;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import fr.cormier.heonLight.HeonLightDOA;
+import org.springframework.context.annotation.*;
 
 import java.time.format.TextStyle;
 
 @Configuration
 @ComponentScan("fr.cormier.heon")
 @Import(BeanConfigurationHeonPixel.class)
+@PropertySource("classpath:application.properties")
 public class BeanConfiguration {
 
     @Bean
     public Test test(){return new Test();}
+
+    @Bean
+    @Scope("prototype")
+    public HeonLightDOA heonLightDOA(){return new HeonLightDOA();};
 
 }
