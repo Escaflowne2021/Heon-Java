3ccd0d51e12d815a3aacf958dae3f37e1c7838d7
gc <cormier.guillaume83@gmail.com>
2019-12-27 18:53:19 +0100

    Socket : reco auto

--- src/main/java/fr/cormier/socket/HeonSocket.java
+++ src/main/java/fr/cormier/socket/HeonSocket.java
@@ -8,33 +8,49 @@ import java.util.HashMap;
 public class HeonSocket {
 
     private Socket socket;
-    private HashMap<String,Socket> socketHashMap = new HashMap<String,Socket>();
+    private HashMap<String, Socket> socketHashMap = new HashMap<String, Socket>();
 
     public HeonSocket() {
         System.out.println("Contructeur HeonSocket ");
     }
 
 
-    public HeonSocket(String IP, int port){
-        try {
-            socket = new Socket(IP,port);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    public HeonSocket(String IP, int port) {
+//
 
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (socket == null || !socket.isConnected()) {
+                    System.out.println("Connexion en Cours ......");
+                    try {
+                        socket = new Socket(IP, port);
+                        socket.setSoTimeout(5*1000); //timeOut de 5s
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+        t.start();
+    }
 
 
     public void SendData(String text) {
         OutputStreamWriter osw;
         try {
-
-            BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
-            bos.write(text.getBytes());
-            bos.flush();
-            //socket.close();
-            System.out.println("Message sent to the server : " + text);
-        } catch (IOException e){
+            if (socket != null && socket.isConnected()) {
+                BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+                bos.write(text.getBytes());
+                bos.flush();
+                //socket.close();
+                System.out.println("Message sent to the server : " + text);
+            } else {
+                System.out.println("Sys Heon non connecté");
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
